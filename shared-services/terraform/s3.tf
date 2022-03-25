ecbce3d6f77519c1dc87f49b6356fa5444710147
ajres <andrew.reslan@mac.com>
2022-03-25 15:18:45 +0000

    customise shared services accounts by creating an S3 bucket

---
+++ shared-services/terraform/s3.tf
@@ -0,0 +1,6 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "shared_services_bucket" {
+  bucket = "shared-services-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
