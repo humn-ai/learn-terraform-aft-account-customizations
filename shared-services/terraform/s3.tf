0612db48af0880851d61a9316b8ee6eb00ed6fd8
Desmond Ho <desmond@humn.ai>
2022-06-30 08:39:05 +0100

    test: rename bucket and test changes

--- shared-services/terraform/s3.tf
+++ shared-services/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "shared_services_bucket" {
-  bucket = "shared-services-${data.aws_caller_identity.current.account_id}"
+  bucket = "shared-services-${data.aws_caller_identity.current.account_id}-test"
   acl    = "private"
 }
